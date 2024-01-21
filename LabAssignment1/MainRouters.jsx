f6c8fdf56514e314dd59d1ec9dfb83760583ba3c
gavinlikewind <gavinlikewind@gmail.com>
2024-01-21 11:04:11 -0500

    - Add an About Me page, a Projects page, a Services page, and a Contact Me page - add above pages into MainRouter

---
+++ LabAssignment1/MainRouters.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import {Route, Routes} from "react-router-dom"
+import Home from "./pages/Home"
+import AboutMe from "./pages/AboutMe ";
+import Projects from "./pages/Projects";
+import Services from "./pages/Services";
+import ContactMe from "./pages/ContactMe";
+
+const MainRouter = () => {
+    return (
+        <div>
+            <Routes>
+                <Route extact path="/" element={<Home />} />
+                <Route extact path="aboutme" element={<AboutMe />} />
+                <Route extact path="projects" element={<Projects />} />
+                <Route extact path="services" element={<Services />} />
+                <Route extact path="contactme" element={<ContactMe />} />
+            </Routes>
+        </div>
+    );
+
+}
---
+++ LabAssignment1/pages/AboutMe .jsx
@@ -0,0 +1,5 @@
+export default function AboutMe(){
+    return(
+        <p>About Me</p>
+    );
+}
